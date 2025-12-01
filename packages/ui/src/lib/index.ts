f5a665e6f879f3d347909dbdd8bcb0f7c7f04b9b
Eduard Klinger <eduardklinger@users.noreply.github.com>
2025-12-01 15:36:03 +0100

    asset and config loading update

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -2,6 +2,7 @@
 // This file serves as the public API for lib utilities available via '@workspace/ui/lib'.
 
 export * from './utils'; // Exporting all from utils.ts
-export * from './asset-url';
+// Re-export from @workspace/utils for convenience
+export { resolveAssetUrl, resolveFirstAssetUrl } from '@workspace/utils';
 
 // TODO: If more utility files are added to src/lib, export them here. 
