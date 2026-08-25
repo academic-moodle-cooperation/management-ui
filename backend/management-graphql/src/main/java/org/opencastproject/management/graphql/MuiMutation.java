91dbede8ca523f3075853a4bbdbcfdb2b073f48b
dominik.kidery <dominik.kidery@tuwien.ac.at>
2026-08-25 16:34:44 +0200

    Fixes checkstyle error

--- backend/management-graphql/src/main/java/org/opencastproject/management/graphql/MuiMutation.java
+++ backend/management-graphql/src/main/java/org/opencastproject/management/graphql/MuiMutation.java
@@ -33,6 +33,9 @@ import org.opencastproject.management.graphql.command.MuiMoveToTrashEventCommand
 import org.opencastproject.management.graphql.command.MuiUpdateEventAclCommand;
 import org.opencastproject.management.graphql.command.MuiUpdateEventCommand;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import graphql.annotations.annotationTypes.GraphQLDefaultValue;
 import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
@@ -40,8 +43,7 @@ import graphql.annotations.annotationTypes.GraphQLName;
 import graphql.annotations.annotationTypes.GraphQLNonNull;
 import graphql.schema.DataFetchingEnvironment;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 @GraphQLName(MuiMutation.TYPE_NAME)
 public class MuiMutation {
